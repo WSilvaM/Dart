d8301ea93bb7efbf3e7242866aa46965552c40f6
WsilvaM <wadst25@gmail.com>
2021-10-26 21:06:55 -0300

    first commit

---
+++ variaveis.dart
@@ -0,0 +1,41 @@
+void main (){
+  //Variaveis que armazena valores inteiros
+  int idade = 33; 
+  print("Idade: $idade");
+
+  int nu1 = 2;
+  int nu2 = 5;
+  print(nu2+nu1);
+
+  //variaveis que armazena numeros decimais
+double raio = 10.25;
+double raio1 = 12.45;
+
+print("Raio: $raio");
+
+
+//variaveis que armazena texto
+String nome = "wanderson";
+print("$nome");
+
+
+bool ligado = true;
+print("Ligado $ligado");
+
+List livros = ["codiguin, livro2, livro3"];
+ print(livros);
+
+List pessoa = ["wanderson, 24 anos,1.70 altura"];
+
+print("pessoa: $pessoa");
+
+//variaveis que armazena valores double em uma lista
+List<double> numeros = [10.32, 20.4, 2.50, 12.45, 32.45];
+print(numeros[2]);
+
+
+
+
+
+
+}
