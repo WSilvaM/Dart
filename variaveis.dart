efa98c0544be16871067b7f58fed3ab764462d5b
WsilvaM <wadst25@gmail.com>
2021-10-27 21:42:51 -0300

    LOGICA EM DART

--- variaveis.dart
+++ variaveis.dart
@@ -33,9 +33,58 @@ print("pessoa: $pessoa");
 List<double> numeros = [10.32, 20.4, 2.50, 12.45, 32.45];
 print(numeros[2]);
 
+// Repetição de 0 a 5 (conhecendo o numero inicial e final)
+//for (inicio condição incremento)
 
+for (int i = 1; i < 5; i++){
+  print([i]);
+}
 
+List<int> numero = [1,2,3,4,5];
+for(int i = 0; i < numero.length; i++){
+  print(numero[i]);
+}
+// List de nomes de pessoas imprimindo os nomes abaixo um do outro
+print("Lista de nomes:");
+List<String> nomes = ["Wanderson","Lucas","bruno","Tatiane","Simone"];
+for(int i = 0; i < numero.length; i++){
+  
+  print(nomes [i]);
+}
+//Repetição de 0 a 5
+//Inicio;WHILE (Condição){Incremento;}
+int j = 0; //inicio
+while(j < 5){ //condição
+ print(j);
+  j++;  //incremento
+}
 
+//Repetição de 0 a 5
+//inicio;do{incremento;} While(Condicao);
 
+int k = 0;
+do{
+  print(k);
+  k++;
+
+}while (k < 5);
+//
+List dados = [1,2,6,4,7];
+//FOREACH
+//FOR (VARIAVEL DENTRO DO CONJUNTO)
+for (int valor in dados){
+  print(valor);
+}
+// Cria uma lista de 10 numeros e imprimir apenas o 
+//Valor da posição 6
+List<int> listNumeros = [0, 47, 52, 69, 55, 44, 27, 11, 78, 58];
+
+ for(int i = 0; i < listNumeros.length; i++){
+
+if(i==5)
+ print(listNumeros [i]);
+ }
+
+  
 
 }
